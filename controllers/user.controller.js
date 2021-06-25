c723445ba23dfdda52463841ddeb3dfddbc864f4
ZSabakh <zsabakhtarishvili@atu.edu>
2021-06-25 14:17:05 +0400

    Users can send admin defined sample sos messages

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -3,6 +3,7 @@ const db = require("../models");
 const Contact = db.contact;
 const Message = db.message;
 const User = db.user;
+const GeneralText = db.general_text;
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 const client = require("twilio")(accountSid, authToken);
@@ -64,20 +65,28 @@ exports.sendText = (req, res) => {
   let phoneData = [];
   let userPhone;
   let userName;
-  let location = `https://www.google.com/maps/place/${req.body.location[0]}+${req.body.location[1]}/`;
+  let location = req.body.location ? `https://www.google.com/maps/place/${req.body.location[0]}+${req.body.location[1]}/` : null;
   let message;
   const queries = req.body.phone.map(async (number) => {
     await User.findOne({ _id: mongoose.Types.ObjectId(req.userId) }, (err, user) => {
       userPhone = user.phone;
       userName = user.username;
     });
-    message = `SOS SENT FROM ${userPhone} with username ${userName}\n\nTEXT: ${req.body.text}\n\n ${req.body.location ? `LOCATION: ${location}` : ""}`;
+
+    if (req.body.general_text_id) {
+      await GeneralText.findOne({ _id: req.body.general_text_id }, (err, textMessage) => {
+        message = `SOS SENT FROM ${userPhone} with username ${userName}\n\nTEXT: ${textMessage.text}\n\n ${req.body.location ? `LOCATION: ${location}` : ""}`;
+      });
+    } else {
+      message = `SOS SENT FROM ${userPhone} with username ${userName}\n\nTEXT: ${req.body.text}\n\n ${req.body.location ? `LOCATION: ${location}` : ""}`;
+    }
     client.messages.create({
       body: message,
       from: "+19707103180",
       to: number,
     });
-    console.log(req.body.location);
+
+    //Find the recepient in all contacts to save in database.
     await Contact.findOne({ phone: number, user_id: mongoose.Types.ObjectId(req.userId) }, (err, contact) => {
       if (err) {
         res.status(500).send({ message: err });
